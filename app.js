75585674871dc99a0266109f2d5e42b491c592b9
Sankar4595 <95sankar@gmail.com>
2023-03-01 00:18:33 +0530

    currencyapi

---
+++ app.js
@@ -0,0 +1,35 @@
+// var api_key = '4d687f2afb184b8fbf52e56d8309deda';
+
+
+async function api(){
+    var price_api = 'https://api.currencyfreaks.com/latest?apikey=4d687f2afb184b8fbf52e56d8309deda';
+    var url = fetch(price_api);
+    var prom = await (await url).json();
+    console.log(prom);
+
+    var parent = document.querySelector('.container');
+    var price_data = document.createElement('p');
+    price_data.innerHTML="Date: "+prom.date;
+    parent.append(price_data);
+
+    //Base
+    var currency_base = document.createElement('p');
+    currency_base.innerHTML = "Base: " + prom.base + " 1 Dollar";
+    parent.append(currency_base);
+
+    //Rates
+    var out = "";
+    var placeholder = document.querySelector('.output');
+    Object.entries(prom.rates).forEach(([keys,value])=>{
+        out+=`
+        <tr>
+            <td class="keys">${keys}</td>
+            <td>${value}</td>
+        </tr>
+        `;
+        return out;
+    })
+    placeholder.innerHTML = out;
+}
+
+api()
